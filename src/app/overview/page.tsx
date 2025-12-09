7f0bb0effedb646423b06976a112307c4286ef3d
IMAC <imac@IMACs-MacBook-Pro.local>
2025-12-09 22:04:32 +0100

    added

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,7 +1,7 @@
 import DashboardLayout from "@/components/templates/DashboardLayout/DashboardLayout";
 
 const OverviewPage = () => {
-  return <DashboardLayout title="Overview">OverviewPageee</DashboardLayout>;
+  return <DashboardLayout title="Overview">OverviewPage</DashboardLayout>;
 };
 
 export default OverviewPage;
