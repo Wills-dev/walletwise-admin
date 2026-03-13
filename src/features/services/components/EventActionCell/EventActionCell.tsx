915e6854121010280efce9b6d96c09880444e424
victor <ejioguvictor6@gmail.com>
2026-03-13 10:14:40 +0100

    added: delete event hooks

--- src/features/services/components/EventActionCell/EventActionCell.tsx
+++ src/features/services/components/EventActionCell/EventActionCell.tsx
@@ -4,8 +4,13 @@ import Link from "next/link";
 
 import ColumnActionDropdown from "@/components/molecules/ColumnActionDropdown/ColumnActionDropdown";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { useDeleteEvent } from "../../hooks/useDeleteEvent";
+import ConfirmAction from "@/components/molecules/ConfirmAction/ConfirmAction";
 
 const EventActionCell = ({ eventId }: { eventId: string }) => {
+  const { isOpen, onCancel, isPending, setIsOpen, deleteEvent } =
+    useDeleteEvent();
+
   return (
     <>
       <ColumnActionDropdown>
@@ -16,9 +21,26 @@ const EventActionCell = ({ eventId }: { eventId: string }) => {
           <Link href={`/services/events/info/${eventId}/edit`}>Edit Event</Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <button className="text-red-500 cursor-pointer">Delete Event</button>
+          <button
+            className="text-red-500 cursor-pointer"
+            onClick={(e) => {
+              e.preventDefault();
+              setIsOpen(true);
+            }}
+          >
+            Delete Event
+          </button>
         </DropdownMenuItem>
       </ColumnActionDropdown>
+      <ConfirmAction
+        isPending={isPending}
+        open={isOpen}
+        setOpen={setIsOpen}
+        onCancel={onCancel}
+        onConfirm={() => deleteEvent({ eventId })}
+        title="Delete event"
+        description="Are you sure you want to delete this event?"
+      />
     </>
   );
 };
