0e5cd65b7602433d3f59e66d9dc819e5fd347d31
wills-dev <ejioguvictor6@gmail.com>
2026-04-16 23:24:02 +0100

    update: commission page restriction removed

--- src/app/commission/page.tsx
+++ src/app/commission/page.tsx
@@ -3,21 +3,10 @@ import { Suspense } from "react";
 import MainLoader from "@/components/atoms/MainLoader/MainLoader";
 import UserCommissionWrapper from "@/components/organisms/UserCommissionWrapper/UserCommissionWrapper";
 
-import { ProtectedPage } from "@/components/ProtectedPage";
-
 const CommissionPage = () => {
   return (
     <Suspense fallback={<MainLoader />}>
-      <ProtectedPage
-        requiredPermissions={[
-          "admin_management.read",
-          "admin_management.write",
-          "admin_management.create",
-        ]}
-        requireAll={false}
-      >
-        <UserCommissionWrapper />
-      </ProtectedPage>
+      <UserCommissionWrapper />
     </Suspense>
   );
 };
